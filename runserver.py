be49a8b6458102c492bbc6ed00e1dd301b778e2e
florije1988 <florije1988@gmail.com>
2015-01-27 17:52:26 +0800

    添加必要文件。

--- runserver.py
+++ runserver.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
+
+from todo import create_app
+
+app = create_app('development')
+
+
+if __name__ == '__main__':
+    app.run(port=app.config.get('PORT'), host=app.config.get('HOST'))
